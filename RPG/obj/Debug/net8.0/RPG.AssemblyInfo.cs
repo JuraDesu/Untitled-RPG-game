048b686f8a1d355c39240c4ecb772fe8a6bf73bc
JuraDesu <145162747+JuraDesu@users.noreply.github.com>
2023-12-09 21:27:53 +0100

    Nothing

--- RPG/obj/Debug/net8.0/RPG.AssemblyInfo.cs
+++ RPG/obj/Debug/net8.0/RPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2b05ff1695eb264b11239daf45f729c00019c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d88a803a842b8a646125472661d5aa284523f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
